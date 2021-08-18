4e6131fae24796d300ea0451f0c0c9d0b78720fd
Papillon6814 <so1rou.k@gmail.com>
2021-08-19 02:45:46 +0900

    feat: red_cardの配置

--- valo_generator.py
+++ valo_generator.py
@@ -5,29 +5,30 @@ class ValoGenerator:
     def __init__(self):
         self.base_height = 900
         self.base_width = 1650
+        self.card_height = 0
+        self.card_width = 0
 
     def gen_base(self):
         blank = cv2.imread("assets/blank.png")
         # blank = np.zeros((self.base_height, self.base_width, 4))
         # blank += [236, 240, 243, 255][::-1]
         print(blank.shape)
-        blank = self.paste_red(blank)
 
+        for i in range(5):
+            blank = self.paste_red(blank, i*(self.card_width+20))
+        
         cv2.imwrite("blank.png", blank)
 
-    def paste_red(self, base):
+    def paste_red(self, base, x):
         red = cv2.imread("assets/redCard.png", -1)
 
         h, w = red.shape[:2]
+        self.card_height = h
+        self.card_width = w
 
-        # mask = red[:, :, 3]
-        # mask = cv2.cvtColor(mask, cv2.COLOR_GRAY2BGR)
-        # mask = mask / 255
+        print(h, w)
 
-        #red = red[:, :, 3]
-        #base[0:w, 0:h, :] = red
-
-        base[0:h, 0:w] = base[0:h, 0:w] * (1 - red[:, :, 3:] / 255) + \
+        base[20:h+20, x+20:x+w+20] = base[20:h+20, x+20:x+w+20] * (1 - red[:, :, 3:] / 255) + \
             red[:, :, :3] * (red[:, :, 3:] / 255)
 
         return base
